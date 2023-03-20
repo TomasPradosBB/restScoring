ad1a88e29f94573d8adfe078c37fb9999e93a1e8
Tomas Prados <tomas.prados@babelgroup.com>
2023-03-20 12:19:58 +0100

    Initial Commit

---
+++ src/main/java/com/helloworld/ServicioRestScoring/ServicioRestScoringApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.ServicioRestScoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioRestScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioRestScoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworld/ServicioRestScoring/ServicioRestScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.ServicioRestScoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioRestScoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
